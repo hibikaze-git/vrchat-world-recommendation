09842847445fa5f2190f165adfbd8e14abd36c67
hibikaze-git <hrk.ymgch@gmail.com>
2022-06-08 23:25:29 +0900

    ログイン・ログアウトの仕組みを追加

--- django/myproject/accounts/urls.py
+++ django/myproject/accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
@@ -9,4 +10,10 @@ urlpatterns = [
 
     path('signup_success/',
         views.SignUpSuccessView.as_view(), name='signup_success'),
+
+    path('login/',
+        auth_views.LoginView.as_view(template_name="login.html"), name='login'),
+
+    path('logout/',
+        auth_views.LogoutView.as_view(template_name="logout.html"), name='logout'),
 ]
