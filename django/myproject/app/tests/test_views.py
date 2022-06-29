dd94906622f3572925250e63684804675c5e9883
hibikaze-git <hrk.ymgch@gmail.com>
2022-06-29 21:44:34 +0900

    トップページのテストを追加

--- django/myproject/app/tests/test_views.py
+++ django/myproject/app/tests/test_views.py
@@ -14,3 +14,16 @@ class UpdateTwitterPostTests(TestCase):
         response = self.client.get('/twitter/recent/?test=true')
 
         self.assertRedirects(response, '/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
+
+
+class IndexViewTests(TestCase):
+    """
+    トップページのテスト
+    """
+    def setUp(self):
+        self.client = Client()
+
+    def test_index(self):
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
