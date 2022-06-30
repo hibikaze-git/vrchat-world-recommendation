12c2c6b042ca31a56b02686b9ef9f232245f71e4
hibikaze-git <hrk.ymgch@gmail.com>
2022-06-30 21:14:35 +0900

    パスワードリセット関連のテストを追加

--- django/myproject/accounts/tests/test_urls.py
+++ django/myproject/accounts/tests/test_urls.py
@@ -1,55 +1,69 @@
+from django.contrib.auth import views as auth_views
 from django.test import TestCase
 from django.urls import resolve
-from django.contrib.auth import views as auth_views
-from ..views import SignUpView, SignUpSuccessView, UserUpdateView
+
 from ..models import CustomUser
+from ..views import SignUpView, SignUpSuccessView, UserUpdateView
 
 
 # Create your tests here.
 class TestUrls(TestCase):
+    """
+    ユーザー情報関連のURLのテスト
+    """
 
     def test_sign_up(self):
-        """
-        signupのurlをテスト
-        """
         view = resolve('/signup/')
         self.assertEqual(view.func.view_class, SignUpView)
 
     def test_sigun_up_success(self):
-        """
-         signup_successのurlをテスト
-        """
         view = resolve('/signup_success/')
         self.assertEqual(view.func.view_class, SignUpSuccessView)
 
     def test_login(self):
-        """
-         loginのurlをテスト
-        """
         view = resolve('/login/')
         self.assertEqual(view.func.view_class, auth_views.LoginView)
 
     def test_logout(self):
-        """
-         logoutのurlをテスト
-        """
         view = resolve('/logout/')
         self.assertEqual(view.func.view_class, auth_views.LogoutView)
 
     def test_update_user(self):
         """
-         updateのurlをテスト
+        ユーザー情報を更新するurlをテスト
         """
         user_name = "user"
         email = "user@mail.com"
         password = "usertestcreatepass"
 
-        custom_user = CustomUser()
-        custom_user.username = user_name
-        custom_user.email = email
-        custom_user.password = password
+        custom_user = CustomUser(
+            username=user_name,
+            email=email,
+            password=password
+        )
 
         custom_user.save()
 
         view = resolve(f'/update/{custom_user.pk}')
         self.assertEqual(view.func.view_class, UserUpdateView)
+
+
+class PasswordResetUrls(TestCase):
+    """
+    パスワードリセット用URLのテスト
+    """
+    def test_reset(self):
+        view = resolve('/password_reset/')
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetView)
+
+    def test_done(self):
+        view = resolve('/password_reset/done/')
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetDoneView)
+
+    def test_confirm(self):
+        view = resolve('/reset/0c6692f4-5788-c69c-6289-0513b949bb47/dsaliefunpofidsdk/')
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetConfirmView)
+
+    def test_complete(self):
+        view = resolve('/reset/done/')
+        self.assertEqual(view.func.view_class, auth_views.PasswordResetCompleteView)
--- django/myproject/myproject/urls.py
+++ django/myproject/myproject/urls.py
@@ -27,13 +27,14 @@ urlpatterns = [
     # accounts用
     path('', include('accounts.urls')),
 
+    # パスワードリセット関連
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name="password_reset.html"), name='password_reset'),
 
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
         template_name="password_reset_sent.html"), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
         template_name="password_reset_form.html"), name='password_reset_confirm'),
 
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
