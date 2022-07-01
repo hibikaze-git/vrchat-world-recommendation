869e6c6dbb0d14040ac87fcaaaeb679f866523d4
hibikaze-git <hrk.ymgch@gmail.com>
2022-07-01 22:19:09 +0900

    like_viewをリファクタリング

--- django/myproject/app/views/index.py
+++ django/myproject/app/views/index.py
@@ -2,7 +2,6 @@
 トップページのview
 """
 from django.views.generic import ListView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from ..models import TwitterCategory, TwitterLike, TwitterPost, TwitterVisit
 
@@ -12,7 +11,7 @@ class IndexView(ListView):
     トップページのビュー
     """
 
-    template_name = 'index.html'
+    template_name = "index.html"
 
     context_object_name = "orderby_records"
 
@@ -29,8 +28,9 @@ class IndexView(ListView):
 
         context["liked_objects"] = TwitterLike.objects.filter(user=self.request.user.id)
 
-        if self.request.GET.get('search_word') is not None:
-            context["search_word"] = self.request.GET.get('search_word')
+        # クエリ
+        if self.request.GET.get("search_word") is not None:
+            context["search_word"] = self.request.GET.get("search_word")
 
         return context
 
@@ -38,8 +38,9 @@ class IndexView(ListView):
 
         queryset = TwitterPost.objects.order_by("-created_at")
 
-        if self.request.GET.get('search_word') is not None:
-            search_word = self.request.GET.get('search_word')
+        # 検索クエリがある場合には絞り込む
+        if self.request.GET.get("search_word") is not None:
+            search_word = self.request.GET.get("search_word")
 
             if search_word != "":
                 queryset = queryset.filter(text__icontains=search_word).order_by("-created_at")
@@ -52,12 +53,13 @@ class IndexSearchView(ListView):
     トップページのビュー(検索)
     """
 
-    template_name = 'contents.html'
+    template_name = "contents.html"
 
     context_object_name = "orderby_records"
 
     paginate_by = 25
 
+    # postを有効化
     def post(self, request, *args, **kwargs):
         return self.get(request, *args, **kwargs)
 
@@ -72,25 +74,27 @@ class IndexSearchView(ListView):
 
         context["liked_objects"] = TwitterLike.objects.filter(user=self.request.user.id)
 
-        if self.request.POST.get('search_word') is not None:
-            context["search_word"] = self.request.POST.get('search_word')
+        # クエリ
+        if self.request.POST.get("search_word") is not None:
+            context["search_word"] = self.request.POST.get("search_word")
 
-        if self.request.POST.get('range') is not None:
-            context["range"] = self.request.POST.get('range')
+        if self.request.POST.get("range") is not None:
+            context["range"] = self.request.POST.get("range")
 
-        if self.request.POST.get('categories') is not None:
-            context["categories"] = self.request.POST.get('categories')
+        if self.request.POST.get("categories") is not None:
+            context["categories"] = self.request.POST.get("categories")
 
         return context
 
     def get_queryset(self):
-        search_word = self.request.POST.get('search_word')
-        range = self.request.POST.get('range')
-        categories = self.request.POST.get('categories')
+        search_word = self.request.POST.get("search_word")
+        range = self.request.POST.get("range")
+        categories = self.request.POST.get("categories")
 
         queryset = TwitterPost.objects.order_by("-created_at")
 
-        if self.request.POST.get('range') is not None or range != "all":
+        # 範囲で絞り込む
+        if self.request.POST.get("range") is not None or range != "all":
 
             if range == "like":
                 liked_id_list = TwitterLike.objects.filter(user=self.request.user).values_list("twitter_post", flat=True)
@@ -100,27 +104,19 @@ class IndexSearchView(ListView):
                 visited_id_list = TwitterVisit.objects.filter(user=self.request.user).values_list("twitter_post", flat=True)
                 queryset = queryset.filter(pk__in=visited_id_list).order_by("-created_at")
 
+        # カテゴリで絞り込む
         if categories != "" and categories is not None:
             categories = categories.split(",")
             category_list = TwitterCategory.objects.filter(pk__in=categories)
             liked_id_list = TwitterLike.objects.filter(user=self.request.user, category__in=category_list).values_list("twitter_post", flat=True)
             queryset = queryset.filter(pk__in=liked_id_list).order_by("-created_at")
 
+        # 検索ワードで絞り込む
         if search_word != "" and search_word is not None:
             queryset = queryset.filter(text__icontains=search_word).order_by("-created_at")
-
-        paginator = Paginator(queryset, self.paginate_by)
-        page = self.request.POST.get('page')
-
-        try:
-            page_obj = paginator.page(page)
-        except PageNotAnInteger:
-            page_obj = paginator.page(1)
-        except EmptyPage:
-            page_obj = paginator.page(paginator.num_pages)
 
         return queryset
 
     def get_paginate_by(self, queryset):
 
-        return self.request.POST.get('paginate_by', IndexSearchView.paginate_by)
+        return self.request.POST.get("paginate_by", IndexSearchView.paginate_by)
--- django/myproject/app/views/like.py
+++ django/myproject/app/views/like.py
@@ -8,7 +8,7 @@ from ..models import TwitterCategory, TwitterLike, TwitterPost
 
 def like_view(request):
     if request.method == "POST":
-        twitter_post = get_object_or_404(TwitterPost, pk=request.POST.get('twitter_post_id'))
+        twitter_post = get_object_or_404(TwitterPost, pk=request.POST.get("twitter_post_id"))
         user = request.user
         like = TwitterLike.objects.filter(twitter_post=twitter_post, user=user)
 
@@ -18,16 +18,13 @@ def like_view(request):
             like.create(twitter_post=twitter_post, user=user)
 
         context = {
-            'twitter_post_id': twitter_post.id,
-            'record': twitter_post
+            "twitter_post_id": twitter_post.id,
+            "record": twitter_post,
+            "liked_list": list(TwitterLike.objects.filter(user=user).values_list("twitter_post", flat=True)),
+            "category_objects": TwitterCategory.objects.filter(user=user),
+            "liked_objects": TwitterLike.objects.filter(user=user)
         }
 
-        context["liked_list"] = list(TwitterLike.objects.filter(user=user).values_list("twitter_post", flat=True))
-
-        context["category_objects"] = TwitterCategory.objects.filter(user=user)
-
-        context["liked_objects"] = TwitterLike.objects.filter(user=user)
-
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+    if request.headers.get("x-requested-with") == "XMLHttpRequest":
 
         return render(request, template_name="like.html", context=context)
