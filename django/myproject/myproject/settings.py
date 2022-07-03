624f311e18d16256fb33aabc1af7ae4ef7ace98b
hibikaze-git <hrk.ymgch@gmail.com>
2022-07-03 15:50:12 +0900

    secret_keyを環境変数から読み込むようにした

--- django/myproject/myproject/settings.py
+++ django/myproject/myproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ve&zbvze556am=cu43=893f5z#xv4_5i_3s1g0osxjnc%#ziw'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
