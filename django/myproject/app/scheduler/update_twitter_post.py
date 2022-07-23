609493da5d601a3e6388a7b86d22b0b051722845
hibikaze-git <hrk.ymgch@gmail.com>
2022-07-23 16:22:45 +0900

    定期実行が行われたに不具合を修正

--- django/myproject/app/scheduler/update_twitter_post.py
+++ django/myproject/app/scheduler/update_twitter_post.py
@@ -1,3 +1,5 @@
+import datetime
+
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from ..views import twitter_view
@@ -7,9 +9,12 @@ def start():
     """
     最新の投稿を定期的に取得
     """
+    # 日時を設定
+    today = datetime.date.today()
+    start_time = datetime.datetime(today.year, today.month, today.day, 0, 30) + datetime.timedelta(days=1)
+
     scheduler = BackgroundScheduler()
 
-    scheduler.add_job(twitter_view.create_twitter_post, 'cron', hour=0, minute=30)
-    scheduler.add_job(twitter_view.create_twitter_post, 'cron', hour=12, minute=30)
+    scheduler.add_job(twitter_view.create_twitter_post, 'interval', minutes=1, start_date=start_time)
 
     scheduler.start()
